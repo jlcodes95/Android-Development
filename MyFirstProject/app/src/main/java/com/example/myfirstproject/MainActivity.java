3cd081b0f4b0260097db59893f3ff8b016ccc036
John Lin <xh.j.lin@gmail.com>
2020-01-28 03:51:55 -0500

    changes to MyFirstProject

--- MyFirstProject/app/src/main/java/com/example/myfirstproject/MainActivity.java
+++ MyFirstProject/app/src/main/java/com/example/myfirstproject/MainActivity.java
@@ -3,20 +3,33 @@ package com.example.myfirstproject;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String USD_TO_CAD = "Enter USD Amount";
-    private static final String CAD_TO_USD = "Enter CAD Amount";
-    private static final double USD_TO_CAD_RATE = 1.25;
-    private static final double CAD_TO_USD_RATE = 0.75;
+    private static final String USD = "USD";
+    private static final String CAD = "CAD";
+    private static final double USD_TO_CAD_RATE = 1.32;
+    private static final double CAD_TO_USD_RATE = 0.76;
+
+    private static DecimalFormat df = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void labelChange(View view){
+        if (((Switch) findViewById(R.id.swU2C)).isChecked()) {
+            ((TextView) findViewById(R.id.tvPrompt)).setText("Enter " + USD + " amount");
+        }else{
+            ((TextView) findViewById(R.id.tvPrompt)).setText("Enter " + CAD + " amount");
+        }
     }
 
     public void convertButtonPressed(View view){
@@ -25,10 +38,20 @@ public class MainActivity extends AppCompatActivity {
             TextView amtTextBox = (TextView) findViewById(R.id.etAmount);
 
             //calculate
-            double convertedAmt = Double.parseDouble(amtTextBox.getText().toString()) * 0.75;
+            double convertedAmt;
+            String targetCountry;
+            //USD TO CAD
+            if (((Switch) findViewById(R.id.swU2C)).isChecked()){
+                convertedAmt = Double.parseDouble(amtTextBox.getText().toString()) * USD_TO_CAD_RATE;
+                targetCountry = CAD;
+            }else{
+                convertedAmt = Double.parseDouble(amtTextBox.getText().toString()) * CAD_TO_USD_RATE;
+                targetCountry = USD;
+            }
+
 
             //display result
-            ((TextView) findViewById(R.id.tvResult)).setText("Converted USD Amount is: $" + convertedAmt);
+            ((TextView) findViewById(R.id.tvResult)).setText("Converted " + targetCountry + " Amount is: $" + df.format(convertedAmt));
         }catch(Exception e){
             Toast t = Toast.makeText(this, "Please Only enter numbers...", Toast.LENGTH_SHORT);
             t.show();
