a39db7d3d24c7484f0fdf257c7bcfe7308f7fddf
John Lin <xh.j.lin@gmail.com>
2020-02-04 17:59:31 -0500

    Menu bar example

---
+++ TestingMenuBar/app/src/main/java/com/example/testingmenubar/AccountProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.testingmenubar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_profile);
+    }
+}
---
+++ TestingMenuBar/app/src/main/java/com/example/testingmenubar/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.testingmenubar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Log.d("MENU", ""+item.getTitle());
+        Toast t;
+        switch (item.getItemId()){
+            case R.id.miAccountProfile:
+                startActivity(new Intent(getApplicationContext(), AccountProfileActivity.class));
+                return true;
+            case R.id.miContact:
+                t = Toast.makeText(getApplicationContext(), "You clicked on Contact", Toast.LENGTH_SHORT);
+                t.show();
+                return true;
+            case R.id.miNewMessage:
+                t = Toast.makeText(getApplicationContext(), "You clicked on Message", Toast.LENGTH_SHORT);
+                t.show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
